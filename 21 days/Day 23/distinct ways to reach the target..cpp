df5a266a49bbe71ccc38ec37cd88c412728d071b
mehtatanuj <54689199+mehtatanuj@users.noreply.github.com>
2020-10-27 23:49:26 +0530

    Add files via upload

---
+++ 21 days/Day 23/distinct ways to reach the target..cpp
@@ -0,0 +1,9 @@
+for (int i = 1; i <= target; ++i) {
+   for (int j = 0; j < ways.size(); ++j) {
+       if (ways[j] <= i) {
+           dp[i] += dp[i - ways[j]];
+       }
+   }
+}
+ 
+return dp[target]
